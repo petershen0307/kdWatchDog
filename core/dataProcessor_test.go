fc38861dd0d93ec3908c457de36f27bd6a711353
PeterShen <petershen0307@hotmail.com>
2019-01-31 00:18:39 +0800

    add test download price from web

--- core/dataProcessor_test.go
+++ core/dataProcessor_test.go
@@ -1,9 +1,29 @@
 package core
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
+
+func Test_downloadStockPrice_WhenCall_Success(t *testing.T) {
+	testTable := []struct {
+		stockID uint
+		period  pricePeriod
+	}{
+		{stockID: 1234, period: weekPricePeriod},
+	}
+	for _, test := range testTable {
+		t.Run(fmt.Sprintf("stockID:(%v), period:(%v)", test.stockID, test.period), func(t *testing.T) {
+			rawData, err := downloadStockPrice(test.stockID, test.period)
+			if err != nil {
+				t.Errorf("download stock price failed with stock id:(%v), time period:(%v)", test.stockID, test.period)
+			}
+			t.Logf("download string:%s", rawData)
+		})
+	}
+
+}
 
 func Test_parseStockPriceJSON_WhenCall_Success(t *testing.T) {
 	gotStr := `null({"mkt":"10","id":"1234","perd":"d","type":"ta","mem":{"id":"1234","name":"黑松","125":29.95,"126":29.9,"638":0.0,"127":0.0},"ta":[{"t":20180928,"o":31.0,"h":31.0,"l":30.75,"c":30.8,"v":48},{"t":20181001,"o":30.85,"h":30.9,"l":30.75,"c":30.9,"v":78}]});`
@@ -33,11 +53,13 @@ func Test_parseStockPriceJSON_WhenCall_Success(t *testing.T) {
 			},
 		},
 	}
-	stockDailyInfo, err := parseStockPriceJSON(gotStr)
-	if err != nil {
-		t.Errorf("Result was incorrect, got %v", err)
-	}
-	if !reflect.DeepEqual(expectedResult, stockDailyInfo) {
-		t.Errorf("Result was incorrect\ngot: %v\nwant:%v", stockDailyInfo, expectedResult)
-	}
+	t.Run("test download data parsing", func(t *testing.T) {
+		stockDailyInfo, err := parseStockPriceJSON(gotStr)
+		if err != nil {
+			t.Errorf("Result was incorrect, got %v", err)
+		}
+		if !reflect.DeepEqual(expectedResult, stockDailyInfo) {
+			t.Errorf("Result was incorrect\ngot: %v\nwant:%v", stockDailyInfo, expectedResult)
+		}
+	})
 }
