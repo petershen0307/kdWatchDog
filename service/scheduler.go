ae3ddf3304e338426ecb55d8b8fac8a208b8bbfe
PeterShen <petershen0307@hotmail.com>
2019-02-21 00:13:22 +0800

    scheduler is ready for unit test

---
+++ service/scheduler.go
@@ -0,0 +1,62 @@
+package service
+
+import "time"
+
+// JobStruct define the job structure
+type JobStruct func()
+
+// ScheduleJob is a job configuration
+type ScheduleJob struct {
+	JobPeriod      time.Duration
+	JobTriggerTime time.Time //Kitchen     = "3:04PM"
+	JobWork        JobStruct
+}
+
+// Scheduler store many ScheduleJob
+type Scheduler struct {
+	Jobs              []ScheduleJob
+	NumberOfJobWorker uint
+	jobQueue          chan JobStruct
+}
+
+// unit is ms
+const scheduleTimePrecise = 100 * time.Millisecond
+
+func (s *Scheduler) workerFunc() {
+	for {
+		select {
+		case x := <-s.jobQueue:
+			x()
+		}
+	}
+}
+
+func (s *Scheduler) triggerOnTimeJobs() {
+	cTime := time.Now()
+	for _, job := range s.Jobs {
+		if cTime.Unix() >= job.JobTriggerTime.Unix() {
+			s.jobQueue <- job.JobWork
+			job.JobTriggerTime = cTime.Add(job.JobPeriod)
+		}
+	}
+}
+
+// Run is the function to execute the Scheduler
+func (s *Scheduler) Run() {
+	// create the channel as the job queue
+	s.jobQueue = make(chan JobStruct, s.NumberOfJobWorker*100)
+	// init the worker
+	for i := uint(0); i < s.NumberOfJobWorker; i++ {
+		go s.workerFunc()
+	}
+	tick := time.Tick(scheduleTimePrecise)
+	for {
+		select {
+		case <-tick:
+			// check scheduler jobs
+			s.triggerOnTimeJobs()
+		default:
+			// do nothing
+		}
+	}
+}
