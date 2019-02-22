5d4bf521f63435342fc69d43c779d50fe9c0773b
PeterShen <petershen0307@hotmail.com>
2019-02-23 00:40:09 +0800

    add a first job to scheduler

--- main.go
+++ main.go
@@ -4,14 +4,24 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/petershen0307/kdWatchDog/core"
 	"github.com/petershen0307/kdWatchDog/service"
 )
 
 func main() {
 	s := service.Scheduler{
 		Jobs: []service.ScheduleJob{
-			{JobPeriod: time.Second, JobTriggerTime: time.Now(), JobWork: func() {
+			{JobPeriod: 24 * time.Hour, JobTriggerTime: time.Now(), JobWork: func() {
 				fmt.Println("current time: ", time.Now())
+				// stock list
+				stockList := []string{"1722", "1726", "2204", "3388", "006208"}
+				for _, id := range stockList {
+					rawData, err := core.GetStockInfoFromWeb(id, core.WeekPricePeriod)
+					if err != nil {
+						return
+					}
+					r := core.KDCalculator(rawData.PriceInfo, 9)
+				}
 			}},
 		},
 		NumberOfJobWorker: 1,
