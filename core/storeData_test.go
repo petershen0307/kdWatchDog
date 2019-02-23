a68681eb374ba124fc558ea812b33c09b30a7f36
PeterShen <petershen0307@hotmail.com>
2019-02-23 17:22:32 +0800

    send kd value to google sheet

---
+++ core/storeData_test.go
@@ -0,0 +1,12 @@
+package core
+
+import "testing"
+
+func Test_saveDataToGoogleSheet_request_success(t *testing.T) {
+	stockDailyInfo := []KDResult{
+		{Date: 20150121, ClosePrice: 67.25, NHighPrice: 0.0, NLowPrice: 0, RSV: 0.0, K: 50, D: 50},
+		{Date: 20150122, ClosePrice: 67.6, NHighPrice: 0.0, NLowPrice: 0, RSV: 0.0, K: 50, D: 50},
+		{Date: 20150123, ClosePrice: 68.7, NHighPrice: 0.0, NLowPrice: 0, RSV: 0.0, K: 50, D: 50},
+	}
+	SaveDataToGoogleSheet(stockDailyInfo, "i am test")
+}
