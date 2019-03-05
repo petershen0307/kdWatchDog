e862a258ee575fdf4ece0ae06099ecf95ca0737d
PeterShen <petershen0307@hotmail.com>
2019-03-05 08:26:13 +0800

    fixed unit test, because interface changed

--- core/storeData_test.go
+++ core/storeData_test.go
@@ -8,5 +8,5 @@ func Test_saveDataToGoogleSheet_request_success(t *testing.T) {
 		{Date: 20150122, ClosePrice: 67.6, NHighPrice: 0.0, NLowPrice: 0, RSV: 0.0, K: 50, D: 50},
 		{Date: 20150123, ClosePrice: 68.7, NHighPrice: 0.0, NLowPrice: 0, RSV: 0.0, K: 50, D: 50},
 	}
-	SaveDataToGoogleSheet(stockDailyInfo, "i am test")
+	SaveDataToGoogleSheet(stockDailyInfo, "i am test", DailyPricePeriod)
 }
