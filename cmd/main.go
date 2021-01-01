c82075a765290b6cde65591bb6d196ab042d340a
PeterShen <petershen0307@hotmail.com>
2021-01-01 15:40:40 +0800

    add telegram echo bot

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+func main() {
+	var (
+		port      = os.Getenv("PORT")       // sets automatically
+		publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
+		token     = os.Getenv("TOKEN")      // you must add it to your config vars
+	)
+
+	webhook := &tb.Webhook{
+		Listen:   ":" + port,
+		Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
+	}
+
+	pref := tb.Settings{
+		Token:  token,
+		Poller: webhook,
+	}
+
+	b, err := tb.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b.Handle("/hello", func(m *tb.Message) {
+		b.Send(m.Sender, "You entered "+m.Text)
+	})
+
+	b.Start()
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
 	google.golang.org/api v0.7.0
+	gopkg.in/tucnak/telebot.v2 v2.3.5
 )
