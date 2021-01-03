4854b9e8663bfb55fe212f09579dde1c854183a4
PeterShen <petershen0307@hotmail.com>
2021-01-03 17:39:03 +0800

    implement delete command

---
+++ handlers/delStock.go
@@ -0,0 +1,58 @@
+package handlers
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/petershen0307/kdWatchDog/models"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"gopkg.in/mgo.v2/bson"
+	tg "gopkg.in/tucnak/telebot.v2"
+)
+
+const delCommand = "/del"
+
+func getDelStockHandler(responseCallback responseCallbackFunc, collection *mongo.Collection) (string, func(*tg.Message)) {
+	return delCommand, func(m *tg.Message) {
+		stockID := strings.Replace(m.Text, delCommand, "", 1)
+		stockID = strings.TrimSpace(stockID)
+		var responseMsg *string = new(string)
+		*responseMsg = "success"
+
+		defer responseCallback(m.Sender, responseMsg)
+
+		queryUser := models.User{}
+		err := collection.FindOne(
+			context.Background(),
+			bson.M{"user_id": m.Sender.ID},
+		).Decode(&queryUser)
+		if err == mongo.ErrNoDocuments {
+			*responseMsg = "no record"
+			return
+		}
+		if err != nil {
+			*responseMsg = err.Error()
+			log.Printf("Query user(%v) with err = %v", m.Sender.ID, err)
+			return
+		}
+
+		removeIndex := -1
+		for i, v := range queryUser.Stocks {
+			if v == stockID {
+				removeIndex = i
+				break
+			}
+		}
+		if removeIndex != -1 {
+			queryUser.Stocks = append(queryUser.Stocks[0:removeIndex], queryUser.Stocks[removeIndex+1:]...)
+		}
+		_, err = collection.UpdateOne(context.Background(), bson.M{"user_id": m.Sender.ID}, bson.M{"$set": queryUser},
+			options.Update().SetUpsert(true))
+		if err != nil {
+			*responseMsg = fmt.Sprintf("Delete stock(%v) fail (%v)", stockID, err)
+		}
+	}
+}
---
+++ handlers/delStock_test.go
@@ -0,0 +1,106 @@
+package handlers
+
+import (
+	"context"
+	"fmt"
+	"sort"
+	"testing"
+
+	"github.com/petershen0307/kdWatchDog/models"
+	"github.com/petershen0307/kdWatchDog/test"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"gopkg.in/mgo.v2/bson"
+	tg "gopkg.in/tucnak/telebot.v2"
+)
+
+func TestDelStockIDSuite(t *testing.T) {
+	t.Skip()
+	suite.Run(t, new(delHandleTestSuite))
+}
+
+type delHandleTestSuite struct {
+	suite.Suite
+	client     *mongo.Client
+	collection *mongo.Collection
+}
+
+func (s *delHandleTestSuite) SetupSuite() {
+	var err error
+	s.client, err = test.InitDB()
+	assert.NoError(s.T(), err)
+	s.collection = test.GetCollection(s.client, "users")
+}
+
+func (s *delHandleTestSuite) TearDownSuite() {
+	assert.NoError(s.T(), test.Deinit(s.client, "users"))
+}
+
+func (s *delHandleTestSuite) TearDownTest() {
+	assert.NoError(s.T(), test.RemoveDocs(s.collection))
+}
+
+func (s *delHandleTestSuite) Test_getDelStockHandler_delNone() {
+	// arrange
+	gotValue := ""
+	responseCallback := func(to tg.Recipient, what interface{}, options ...interface{}) {
+		gotValue = *what.(*string)
+	}
+	stockID := "1234"
+	userID := 5566
+
+	// act
+	command, f := getDelStockHandler(responseCallback, s.collection)
+	f(&tg.Message{
+		Sender: &tg.User{
+			ID: userID,
+		},
+		Text: fmt.Sprintf("/del %v", stockID),
+	})
+
+	// assert
+	assert.Equal(s.T(), delCommand, command)
+	assert.Equal(s.T(), "no record", gotValue)
+}
+
+func (s *delHandleTestSuite) Test_getDelStockHandler_delOne() {
+	// arrange
+	gotValue := ""
+	responseCallback := func(to tg.Recipient, what interface{}, options ...interface{}) {
+		gotValue = *what.(*string)
+	}
+	mockUser := models.User{
+		UserID: 7788,
+		Stocks: []string{"1101", "2330", "aapl"},
+	}
+	_, err := s.collection.UpdateOne(context.Background(), bson.M{"user_id": mockUser.UserID},
+		bson.M{
+			"$set": mockUser,
+		}, options.Update().SetUpsert(true))
+	assert.NoError(s.T(), err)
+
+	// act
+	command, f := getDelStockHandler(responseCallback, s.collection)
+	f(&tg.Message{
+		Sender: &tg.User{
+			ID: mockUser.UserID,
+		},
+		Text: "/del 1101",
+	})
+
+	// assert
+	actualResult := models.User{}
+	err = s.collection.FindOne(context.Background(), bson.M{"user_id": mockUser.UserID}).Decode(&actualResult)
+	sort.Strings(actualResult.Stocks)
+	expectResult := models.User{
+		UserID: 7788,
+		Stocks: []string{"2330", "aapl"},
+	}
+	sort.Strings(expectResult.Stocks)
+	assert.NoError(s.T(), err)
+	assert.Equal(s.T(), expectResult, actualResult)
+	assert.Equal(s.T(), delCommand, command)
+	assert.Equal(s.T(), "success", gotValue)
+}
