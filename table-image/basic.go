21f7f6b314d03c2d872854b71ab73940449530c3
PeterShen <petershen0307@hotmail.com>
2021-01-21 22:56:36 +0800

    add code from https://github.com/Techbinator/go-table-image

---
+++ table-image/basic.go
@@ -0,0 +1,145 @@
+package tableimage
+
+import (
+	"image"
+	"image/draw"
+
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/basicfont"
+	"golang.org/x/image/math/fixed"
+)
+
+func (ti *tableImage) setRgba() {
+	img := image.NewRGBA(image.Rect(0, 0, ti.width, ti.height))
+	//set image background
+	draw.Draw(img, img.Bounds(), &image.Uniform{getColorByHex(ti.backgroundColor)}, image.ZP, draw.Src)
+	ti.img = img
+}
+
+func (ti *tableImage) addString(x, y int, label string, color string) {
+
+	point := fixed.Point26_6{fixed.Int26_6(x * 64), fixed.Int26_6(y * 64)}
+
+	d := &font.Drawer{
+		Dst:  ti.img,
+		Src:  image.NewUniform(getColorByHex(color)),
+		Face: basicfont.Face7x13,
+		Dot:  point,
+	}
+	d.DrawString(label)
+}
+
+//Thx to https://github.com/StephaneBunel/bresenham
+func (ti *tableImage) addLine(x1, y1, x2, y2 int, color string) {
+
+	var dx, dy, e, slope int
+	col := getColorByHex(color)
+	// Because drawing p1 -> p2 is equivalent to draw p2 -> p1,
+	// I sort points in x-axis order to handle only half of possible cases.
+	if x1 > x2 {
+		x1, y1, x2, y2 = x2, y2, x1, y1
+	}
+
+	dx, dy = x2-x1, y2-y1
+	// Because point is x-axis ordered, dx cannot be negative
+	if dy < 0 {
+		dy = -dy
+	}
+
+	switch {
+
+	// Is line a point ?
+	case x1 == x2 && y1 == y2:
+		ti.img.Set(x1, y1, col)
+
+	// Is line an horizontal ?
+	case y1 == y2:
+		for ; dx != 0; dx-- {
+			ti.img.Set(x1, y1, col)
+			x1++
+		}
+		ti.img.Set(x1, y1, col)
+
+	// Is line a vertical ?
+	case x1 == x2:
+		if y1 > y2 {
+			y1, y2 = y2, y1
+		}
+		for ; dy != 0; dy-- {
+			ti.img.Set(x1, y1, col)
+			y1++
+		}
+		ti.img.Set(x1, y1, col)
+
+	// Is line a diagonal ?
+	case dx == dy:
+		if y1 < y2 {
+			for ; dx != 0; dx-- {
+				ti.img.Set(x1, y1, col)
+				x1++
+				y1++
+			}
+		} else {
+			for ; dx != 0; dx-- {
+				ti.img.Set(x1, y1, col)
+				x1++
+				y1--
+			}
+		}
+		ti.img.Set(x1, y1, col)
+
+	// wider than high ?
+	case dx > dy:
+		if y1 < y2 {
+			dy, e, slope = 2*dy, dx, 2*dx
+			for ; dx != 0; dx-- {
+				ti.img.Set(x1, y1, col)
+				x1++
+				e -= dy
+				if e < 0 {
+					y1++
+					e += slope
+				}
+			}
+		} else {
+			dy, e, slope = 2*dy, dx, 2*dx
+			for ; dx != 0; dx-- {
+				ti.img.Set(x1, y1, col)
+				x1++
+				e -= dy
+				if e < 0 {
+					y1--
+					e += slope
+				}
+			}
+		}
+		ti.img.Set(x2, y2, col)
+
+	// higher than wide.
+	default:
+		if y1 < y2 {
+			dx, e, slope = 2*dx, dy, 2*dy
+			for ; dy != 0; dy-- {
+				ti.img.Set(x1, y1, col)
+				y1++
+				e -= dx
+				if e < 0 {
+					x1++
+					e += slope
+				}
+			}
+		} else {
+			dx, e, slope = 2*dx, dy, 2*dy
+			for ; dy != 0; dy-- {
+				ti.img.Set(x1, y1, col)
+				y1--
+				e -= dx
+				if e < 0 {
+					x1++
+					e += slope
+				}
+			}
+		}
+		ti.img.Set(x2, y2, col)
+	}
+}
---
+++ table-image/table.go
@@ -0,0 +1,104 @@
+package tableimage
+
+import (
+	"bytes"
+	"image/jpeg"
+	"image/png"
+	"os"
+)
+
+func (ti *tableImage) drawTH() {
+	for colNo, td := range ti.th.Tds {
+		ti.addString(colNo*columnSpace+tablePadding, 1*rowSpace, td.Text, td.Color)
+		ti.addLine(colNo*columnSpace, 0, colNo*columnSpace, ti.height, "#000000")
+	}
+	//draw the end horizontal line
+	ti.addLine(ti.width-1, 0, ti.width-1, ti.height, "#000000")
+	//draw the start horizontal line
+	ti.addLine(1, 1, ti.width, 1, "#000000")
+	//draw the double line to signal it is a th
+	ti.addLine(1, 1*rowSpace+separatorPadding, ti.width, 1*rowSpace+separatorPadding, "#000000")
+	ti.addLine(1, 1*rowSpace+separatorPadding+2, ti.width, 1*rowSpace+separatorPadding+2, "#000000")
+
+}
+
+func (ti *tableImage) drawTR() {
+	fRowNo := 2
+	for _, tds := range ti.trs {
+		//start with the second row since the first one is the th
+		maxRowHeight := 1
+		for colNo, td := range tds.Tds {
+			wrapedTexts := wrapText(td.Text)
+			noRowPerCol := len(wrapedTexts)
+			if maxRowHeight < noRowPerCol {
+				maxRowHeight = noRowPerCol
+			}
+			for noTextRows, wrapedText := range wrapedTexts {
+				colHeight := (fRowNo + noTextRows) * rowSpace
+				colWidth := colNo*columnSpace + tablePadding
+
+				if colNo == 0 {
+					colWidth = columnSpace - columnSpace + tablePadding
+				}
+
+				ti.addString(colWidth, colHeight, wrapedText, td.Color)
+			}
+		}
+		fRowNo += maxRowHeight
+		ti.addLine(1, (fRowNo-1)*rowSpace+separatorPadding, ti.width, (fRowNo-1)*rowSpace+separatorPadding, "#000")
+	}
+}
+
+func (ti *tableImage) calculateHeight() {
+	//start from 1 since we have th
+	totalRowNo := 1
+	for _, tr := range ti.trs {
+		maxRowHeight := 1
+		for _, td := range tr.Tds {
+			wrapedText := wrapText(td.Text)
+			// in case we have a multi line text
+			if len(wrapedText) > maxRowHeight {
+				maxRowHeight = len(wrapedText)
+			}
+		}
+		totalRowNo += maxRowHeight
+	}
+	ti.height = totalRowNo*rowSpace + rowSpace - tablePadding + 5
+}
+
+func (ti *tableImage) calculateWidth() {
+	totalColumnNo := len(ti.th.Tds)
+
+	ti.width = totalColumnNo * columnSpace
+}
+
+func (ti *tableImage) saveFile() {
+	f, err := os.Create(ti.filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	if ti.fileType == "jpg" {
+		if err := jpeg.Encode(f, ti.img, nil); err != nil {
+			panic(err)
+		}
+	} else {
+		if err := png.Encode(f, ti.img); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func (ti *tableImage) get() *bytes.Buffer {
+	buffer := new(bytes.Buffer)
+	if ti.fileType == "jpg" {
+		if err := jpeg.Encode(buffer, ti.img, nil); err != nil {
+			panic(err)
+		}
+	} else {
+		if err := png.Encode(buffer, ti.img); err != nil {
+			panic(err)
+		}
+	}
+	return buffer
+}
